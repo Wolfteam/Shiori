15c333c9ae6821f16f665fdea92358a8b1c8516c
Efrain <mimo4325@gmail.com>
2021-03-10 20:23:56 -0500

    Workaround for the push / pop of pages problem

--- lib/application/weapon/weapon_event.dart
+++ lib/application/weapon/weapon_event.dart
@@ -4,9 +4,11 @@ part of 'weapon_bloc.dart';
 abstract class WeaponEvent with _$WeaponEvent {
   const factory WeaponEvent.loadFromName({
     @required String key,
+    @Default(true) bool addToQueue,
   }) = _LoadWeaponFromName;
 
   const factory WeaponEvent.loadFromImg({
     @required String image,
+    @Default(true) bool addToQueue,
   }) = _LoadWeaponFromImg;
 }
