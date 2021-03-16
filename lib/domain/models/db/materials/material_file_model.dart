68462a441a47bf71776a1dd9ff83456d4fc214a4
Efrain <mimo4325@gmail.com>
2021-03-15 20:42:38 -0500

    Added the "obtained from" prop to the materials.json file

--- lib/domain/models/db/materials/material_file_model.dart
+++ lib/domain/models/db/materials/material_file_model.dart
@@ -1,7 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-
-import '../../../assets.dart';
-import '../../../enums/material_type.dart';
+import 'package:genshindb/domain/assets.dart';
+import 'package:genshindb/domain/enums/enums.dart';
+import 'package:genshindb/domain/models/models.dart';
 
 part 'material_file_model.freezed.dart';
 part 'material_file_model.g.dart';
@@ -28,6 +28,7 @@ abstract class MaterialFileModel implements _$MaterialFileModel {
     @required MaterialType type,
     @required List<int> days,
     @required double level,
+    @required List<ObtainedFromFileModel> obtainedFrom,
     Map<String, dynamic> attributes,
   }) = _MaterialFileModel;
 
@@ -46,4 +47,15 @@ abstract class ExperienceMaterialAttributesModel implements _$ExperienceMaterial
   ExperienceMaterialAttributesModel._();
 
   factory ExperienceMaterialAttributesModel.fromJson(Map<String, dynamic> json) => _$ExperienceMaterialAttributesModelFromJson(json);
+}
+
+@freezed
+abstract class ObtainedFromFileModel implements _$ObtainedFromFileModel {
+  factory ObtainedFromFileModel({
+    @required List<ItemAscensionMaterialModel> items,
+  }) = _ObtainedFromFileModel;
+
+  ObtainedFromFileModel._();
+
+  factory ObtainedFromFileModel.fromJson(Map<String, dynamic> json) => _$ObtainedFromFileModelFromJson(json);
 }
