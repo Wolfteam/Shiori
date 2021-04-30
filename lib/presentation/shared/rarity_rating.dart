41d4d040d098ae0c5d5f2afec88e654899ad5fb9
Efrain <mimo4325@gmail.com>
2021-04-29 19:43:03 -0500

    Copy pasted the SmoothStarRating package. Switched to flutter_user_agentx

--- lib/presentation/shared/rarity_rating.dart
+++ lib/presentation/shared/rarity_rating.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:smooth_star_rating/smooth_star_rating.dart';
+import 'package:genshindb/presentation/shared/smooth_star_rating.dart';
 
 class RarityRating extends StatelessWidget {
   final int rarity;
