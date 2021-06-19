2c46265b1828d2e5a6f7d2f0b3d302bd9a270710
Efrain <mimo4325@gmail.com>
2021-06-19 12:51:25 -0500

    [Infrastructure] Show the build number in the app version

--- lib/infrastructure/device_info_service.dart
+++ lib/infrastructure/device_info_service.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter_user_agentx/flutter_user_agent.dart';
 import 'package:genshindb/domain/services/device_info_service.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class DeviceInfoServiceImpl implements DeviceInfoService {
   Map<String, String> _deviceInfo;
@@ -28,12 +28,12 @@ class DeviceInfoServiceImpl implements DeviceInfoService {
       final deviceInfo = DeviceInfoPlugin();
       final androidInfo = await deviceInfo.androidInfo;
       final packageInfo = await PackageInfo.fromPlatform();
-      _version = packageInfo.version;
+      _version = '${packageInfo.version}+${packageInfo.buildNumber}';
       _appName = packageInfo.appName;
       _deviceInfo = {
         'Model': androidInfo.model,
         'OsVersion': '${androidInfo.version.sdkInt}',
-        'AppVersion': '${packageInfo.version}+${packageInfo.buildNumber}'
+        'AppVersion': _version,
       };
 
       if (!Platform.isWindows) {
