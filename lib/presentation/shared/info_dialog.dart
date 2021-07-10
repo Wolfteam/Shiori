c38a809b17320ee64991b16303e7d434d6ec5185
Efrain <mimo4325@gmail.com>
2021-07-10 18:00:01 -0500

    Added a media query extension to calculate the dialog width to use

--- lib/presentation/shared/info_dialog.dart
+++ lib/presentation/shared/info_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:genshindb/generated/l10n.dart';
 import 'package:genshindb/presentation/shared/bullet_list.dart';
+import 'package:genshindb/presentation/shared/extensions/media_query_extensions.dart';
 
 class InfoDialog extends StatelessWidget {
   final List<String> explanations;
@@ -16,7 +17,10 @@ class InfoDialog extends StatelessWidget {
     return AlertDialog(
       title: Text(s.information),
       content: SingleChildScrollView(
-        child: BulletList(items: explanations, fontSize: 14),
+        child: SizedBox(
+          width: MediaQuery.of(context).getWidthForDialogs(),
+          child: BulletList(items: explanations, fontSize: 14),
+        ),
       ),
       actions: [
         ElevatedButton(
