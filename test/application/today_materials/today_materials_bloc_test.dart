c95a8b412d3fb3a53db359ef82e29d8795fa066b
Efrain <mimo4325@gmail.com>
2021-10-24 20:53:35 -0500

    [Tests] Added tests to the remaining blocs

---
+++ test/application/today_materials/today_materials_bloc_test.dart
@@ -0,0 +1,55 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:shiori/application/bloc.dart';
+import 'package:shiori/domain/enums/app_language_type.dart';
+import 'package:shiori/domain/services/genshin_service.dart';
+import 'package:shiori/domain/services/telemetry_service.dart';
+import 'package:shiori/infrastructure/infrastructure.dart';
+
+import '../../common.dart';
+import '../../mocks.mocks.dart';
+
+void main() {
+  late final TelemetryService _telemetryService;
+  late final GenshinService _genshinService;
+
+  setUpAll(() {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    _telemetryService = MockTelemetryService();
+    final settingsService = MockSettingsService();
+    when(settingsService.language).thenReturn(AppLanguageType.english);
+    final localeService = LocaleServiceImpl(settingsService);
+    _genshinService = GenshinServiceImpl(localeService);
+
+    return Future(() async {
+      await _genshinService.init(settingsService.language);
+    });
+  });
+
+  test(
+    'Initial state',
+    () => expect(TodayMaterialsBloc(_genshinService, _telemetryService).state, const TodayMaterialsState.loading()),
+  );
+
+  blocTest<TodayMaterialsBloc, TodayMaterialsState>(
+    'Init',
+    build: () => TodayMaterialsBloc(_genshinService, _telemetryService),
+    act: (bloc) => bloc.add(const TodayMaterialsEvent.init()),
+    verify: (bloc) {
+      bloc.state.map(
+        loading: (_) => throw Exception('Invalid state'),
+        loaded: (state) {
+          expect(state.charAscMaterials, isNotEmpty);
+          expect(state.weaponAscMaterials, isNotEmpty);
+          expect(state.charAscMaterials, isNotEmpty);
+          final items = state.charAscMaterials.expand((el) => el.characters).toList() + state.weaponAscMaterials.expand((el) => el.weapons).toList();
+          checkItemsCommon(items);
+
+          final days = (state.charAscMaterials.expand((e) => e.days).toList() + state.weaponAscMaterials.expand((e) => e.days).toList()).toSet();
+          expect(days.length, TodayMaterialsBloc.days.length);
+        },
+      );
+    },
+  );
+}
---
+++ test/application/weapon/weapon_bloc_test.dart
@@ -0,0 +1,95 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shiori/application/bloc.dart';
+import 'package:shiori/domain/enums/enums.dart';
+import 'package:shiori/domain/services/data_service.dart';
+import 'package:shiori/domain/services/genshin_service.dart';
+import 'package:shiori/domain/services/telemetry_service.dart';
+import 'package:shiori/infrastructure/genshin_service.dart';
+import 'package:shiori/infrastructure/infrastructure.dart';
+
+import '../../common.dart';
+import '../../mocks.mocks.dart';
+
+const _dbFolder = 'shiori_weapon_bloc_tests';
+
+void main() {
+  late final TelemetryService _telemetryService;
+  late final GenshinService _genshinService;
+  late final DataService _dataService;
+
+  const _key = 'aquila-favonia';
+
+  setUpAll(() {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    _telemetryService = MockTelemetryService();
+    final settingsService = SettingsServiceImpl(MockLoggingService());
+    final localeService = LocaleServiceImpl(settingsService);
+    _genshinService = GenshinServiceImpl(localeService);
+    _dataService = DataServiceImpl(_genshinService, CalculatorServiceImpl(_genshinService));
+
+    return Future(() async {
+      await _genshinService.init(AppLanguageType.english);
+      await _dataService.init(dir: _dbFolder);
+    });
+  });
+
+  tearDownAll(() {
+    return Future(() async {
+      await _dataService.closeThemAll();
+      await deleteDbFolder(_dbFolder);
+    });
+  });
+
+  test(
+    'Initial state',
+    () => expect(WeaponBloc(_genshinService, _telemetryService, _dataService).state, const WeaponState.loading()),
+  );
+
+  group('Load from key', () {
+    void _checkState(WeaponState state, bool isInInventory) {
+      state.map(
+        loading: (_) => throw Exception('Invalid state'),
+        loaded: (state) {
+          expect(state.key, _key);
+          expect(state.name, 'Aquila Favonia');
+          checkAsset(state.fullImage);
+          checkTranslation(state.description, canBeNull: false);
+          expect(state.rarity, 5);
+          expect(state.atk, 48);
+          expect(state.secondaryStatValue, 9);
+          expect(state.secondaryStat, StatType.physDmgBonusPercentage);
+          expect(state.locationType, ItemLocationType.gacha);
+          expect(state.weaponType, WeaponType.sword);
+          expect(state.isInInventory, isInInventory);
+          expect(state.ascensionMaterials, isNotEmpty);
+          expect(state.refinements, isNotEmpty);
+          expect(state.characters, isNotEmpty);
+          expect(state.stats, isNotEmpty);
+          expect(state.craftingMaterials, isEmpty);
+        },
+      );
+    }
+
+    blocTest<WeaponBloc, WeaponState>(
+      'keqing',
+      build: () => WeaponBloc(_genshinService, _telemetryService, _dataService),
+      act: (bloc) => bloc.add(const WeaponEvent.loadFromKey(key: _key)),
+      //we skip 1 because since the event is not _AddedToInventory the bloc will emit a loading
+      skip: 1,
+      verify: (bloc) => _checkState(bloc.state, false),
+    );
+
+    blocTest<WeaponBloc, WeaponState>(
+      'keqing is in inventory',
+      build: () => WeaponBloc(_genshinService, _telemetryService, _dataService),
+      setUp: () {
+        _dataService.addItemToInventory(_key, ItemType.weapon, 1);
+      },
+      act: (bloc) => bloc.add(const WeaponEvent.loadFromKey(key: _key)),
+      //we skip 1 because since the event is not _AddedToInventory the bloc will emit a loading
+      skip: 1,
+      verify: (bloc) => _checkState(bloc.state, true),
+    );
+  });
+}
