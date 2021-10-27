9722d2376cc2b6c1ec4a8b22d77e529c7bad6b22
Efrain <mimo4325@gmail.com>
2021-10-26 21:42:37 -0500

    Sort the materials shown in the calculator

--- lib/domain/models/materials/material_card_model.dart
+++ lib/domain/models/materials/material_card_model.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:shiori/domain/enums/material_type.dart';
+import 'package:shiori/domain/models/models.dart';
 
 part 'material_card_model.freezed.dart';
 
 @freezed
-class MaterialCardModel with _$MaterialCardModel {
+class MaterialCardModel with _$MaterialCardModel implements SortableGroupedMaterial {
+  @Implements(SortableGroupedMaterial)
   const factory MaterialCardModel.item({
     required String key,
     required String name,
