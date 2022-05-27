9107f68dd6f643aefff254a0fea7033a077f6424
Efrain <mimo4325@gmail.com>
2022-05-26 21:33:15 -0500

    [Application] Minor rename

--- lib/application/birthdays_per_month/birthdays_per_month_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'birthdays_per_month_bloc.dart';
-
-@freezed
-class BirthdaysPerMonthEvent with _$BirthdaysPerMonthEvent {
-  const factory BirthdaysPerMonthEvent.init({
-    required int month,
-  }) = _BirthdaysPerMonthEvent;
-}
