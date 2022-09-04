f8e5cb50e0d46879024b25d9c1368f0b42ea30c2
Efrain Bastidas <mimo4325@gmail.com>
2022-09-04 17:39:25 -0500

    Post merge fixes

--- lib/domain/assets.dart
+++ lib/domain/assets.dart
@@ -1,3 +1,4 @@
+import 'package:shiori/domain/app_constants.dart';
 import 'package:shiori/domain/enums/enums.dart';
 
 class Assets {
@@ -5,33 +6,33 @@ class Assets {
   static String otherImgsBasePath = 'assets/others';
   static String weaponTypesBasePath = 'assets/weapon_types';
 
-  static String noImageAvailablePath = '$otherImgsBasePath/na.png';
-  static String paimonImagePath = '$otherImgsBasePath/paimon.png';
-  static String bagIconPath = '$otherImgsBasePath/bag.png';
-  static String monsterIconPath = '$otherImgsBasePath/monster.png';
-  static String cakeIconPath = '$otherImgsBasePath/cake.png';
-  static String gachaIconPath = '$otherImgsBasePath/gacha.png';
-  static String starCrystalIconPath = '$otherImgsBasePath/mark_wind_crystal.png';
-  static String primogemIconPath = '$otherImgsBasePath/primogem.png';
+  static String noImageAvailablePath = '$otherImgsBasePath/na$imageFileExtension';
+  static String paimonImagePath = '$otherImgsBasePath/paimon$imageFileExtension';
+  static String bagIconPath = '$otherImgsBasePath/bag$imageFileExtension';
+  static String monsterIconPath = '$otherImgsBasePath/monster$imageFileExtension';
+  static String cakeIconPath = '$otherImgsBasePath/cake$imageFileExtension';
+  static String gachaIconPath = '$otherImgsBasePath/gacha$imageFileExtension';
+  static String starCrystalIconPath = '$otherImgsBasePath/mark_wind_crystal$imageFileExtension';
+  static String primogemIconPath = '$otherImgsBasePath/primogem$imageFileExtension';
 
   static String _getElementPath(String name) => '$elementsBasePath/$name';
 
   static String getElementPathFromType(ElementType type) {
     switch (type) {
       case ElementType.anemo:
-        return _getElementPath('anemo.png');
+        return _getElementPath('anemo$imageFileExtension');
       case ElementType.cryo:
-        return _getElementPath('cryo.png');
+        return _getElementPath('cryo$imageFileExtension');
       case ElementType.dendro:
-        return _getElementPath('dendro.png');
+        return _getElementPath('dendro$imageFileExtension');
       case ElementType.electro:
-        return _getElementPath('electro.png');
+        return _getElementPath('electro$imageFileExtension');
       case ElementType.geo:
-        return _getElementPath('geo.png');
+        return _getElementPath('geo$imageFileExtension');
       case ElementType.hydro:
-        return _getElementPath('hydro.png');
+        return _getElementPath('hydro$imageFileExtension');
       case ElementType.pyro:
-        return _getElementPath('pyro.png');
+        return _getElementPath('pyro$imageFileExtension');
       default:
         throw Exception('Invalid element type = $type');
     }
@@ -44,15 +45,15 @@ class Assets {
   static String getArtifactPathFromType(ArtifactType type) {
     switch (type) {
       case ArtifactType.clock:
-        return '$otherImgsBasePath/clock.png';
+        return '$otherImgsBasePath/clock$imageFileExtension';
       case ArtifactType.crown:
-        return '$otherImgsBasePath/crown.png';
+        return '$otherImgsBasePath/crown$imageFileExtension';
       case ArtifactType.flower:
-        return '$otherImgsBasePath/flower.png';
+        return '$otherImgsBasePath/flower$imageFileExtension';
       case ArtifactType.goblet:
-        return '$otherImgsBasePath/goblet.png';
+        return '$otherImgsBasePath/goblet$imageFileExtension';
       case ArtifactType.plume:
-        return '$otherImgsBasePath/plume.png';
+        return '$otherImgsBasePath/plume$imageFileExtension';
       default:
         throw Exception('Invalid artifact type = $type');
     }
@@ -61,15 +62,15 @@ class Assets {
   static String getWeaponTypePath(WeaponType type) {
     switch (type) {
       case WeaponType.bow:
-        return '$weaponTypesBasePath/bow.png';
+        return '$weaponTypesBasePath/bow$imageFileExtension';
       case WeaponType.catalyst:
-        return '$weaponTypesBasePath/catalyst.png';
+        return '$weaponTypesBasePath/catalyst$imageFileExtension';
       case WeaponType.claymore:
-        return '$weaponTypesBasePath/claymore.png';
+        return '$weaponTypesBasePath/claymore$imageFileExtension';
       case WeaponType.polearm:
-        return '$weaponTypesBasePath/polearm.png';
+        return '$weaponTypesBasePath/polearm$imageFileExtension';
       case WeaponType.sword:
-        return '$weaponTypesBasePath/sword.png';
+        return '$weaponTypesBasePath/sword$imageFileExtension';
       default:
         throw Exception('Invalid weapon type = $type');
     }
