a19cd122e791873e0a041f28616c75adeb15977b
Efrain Bastidas <mimo4325@gmail.com>
2022-09-17 20:38:58 -0500

    [Presentation] Bloc listener may not be triggered on desktop

--- lib/application/main/main_state.dart
+++ lib/application/main/main_state.dart
@@ -2,7 +2,7 @@ part of 'main_bloc.dart';
 
 @freezed
 class MainState with _$MainState {
-  const factory MainState.loading() = _MainLoadingState;
+  const factory MainState.loading({required LanguageModel language}) = _MainLoadingState;
 
   const factory MainState.loaded({
     required String appTitle,
