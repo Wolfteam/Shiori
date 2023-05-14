c31434e7d091dbab3a434079a7daa3ef907ee5db
Efrain Bastidas <mimo4325@gmail.com>
2023-05-14 11:08:21 -0500

    Added intl_utils to dev dependenies so translations are updated on clean

--- run_clean.sh
+++ run_clean.sh
@@ -11,4 +11,7 @@ echo 'Deleting conflicting outputs...'
 fvm flutter pub run build_runner clean
 fvm flutter pub run build_runner build --delete-conflicting-outputs
 
+echo 'Updating generated translations'
+fvm flutter pub run intl_utils:generate
+
 echo 'Clean completed'
