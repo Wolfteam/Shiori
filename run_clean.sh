a6b247e3bff7b8215f98aba301cb98de6fc3a7fb
Efrain Bastidas <mimo4325@gmail.com>
2023-10-04 15:04:34 -0500

    Added a build.yaml to see if we can reduce the build_runner build time

--- run_clean.sh
+++ run_clean.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
 set -e
 
-delete_conflicting_outputs=1
-
-while [[ "$#" -gt 0 ]]; do
-  case $1 in
-  --no_delete_conflicting_outputs) delete_conflicting_outputs=0 ;;
-  *)
-    echo "Unknown parameter passed: $1"
-    exit 1
-    ;;
-  esac
-  shift
-done
-
-
 echo 'Cleaning project...'
 fvm flutter clean
 
 echo 'Retrieving packages...'
 fvm flutter pub get
 
-if [ "$delete_conflicting_outputs" = 1 ]; then
-  echo 'Deleting conflicting outputs...'
-  fvm flutter pub run build_runner clean
-  fvm flutter pub run build_runner build --delete-conflicting-outputs
-fi
+echo 'Deleting conflicting outputs...'
+fvm flutter pub run build_runner clean
+fvm flutter pub run build_runner build --delete-conflicting-outputs
 
 echo 'Updating generated translations'
 fvm flutter pub run intl_utils:generate
