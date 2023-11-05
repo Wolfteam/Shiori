81e385d7516dd10cb3a118db98170121613d422f
Efrain Bastidas <mimo4325@gmail.com>
2023-11-04 22:14:28 -0500

    [Application] Removed not needed blocs

--- lib/application/calculator_asc_materials/sessions_order/calculator_asc_materials_sessions_order_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'calculator_asc_materials_sessions_order_bloc.dart';
-
-@freezed
-class CalculatorAscMaterialsSessionsOrderState with _$CalculatorAscMaterialsSessionsOrderState {
-  const factory CalculatorAscMaterialsSessionsOrderState.initial({
-    required List<CalculatorSessionModel> sessions,
-  }) = _InitialState;
-}
