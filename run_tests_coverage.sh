2e071e1d56f82c45a0bb179cd2ce5fe60b331f83
Efrain Bastidas <mimo4325@gmail.com>
2023-11-20 11:22:36 -0500

    [Tests] Minor changs to the run_tests_coverage.sh

--- run_tests_coverage.sh
+++ run_tests_coverage.sh
@@ -4,9 +4,11 @@ set -e
 CONCURRENCY=$(getconf _NPROCESSORS_ONLN || 4)
 echo "Running tests with concurrency = $CONCURRENCY on $(date)"
 
-fvm flutter test --no-test-assets --no-pub --concurrency="$CONCURRENCY" --coverage
-lcov --remove coverage/lcov.info 'lib/domain/*' 'lib/application/app_bloc_observer.dart' -o coverage/new_lcov.info
-genhtml coverage/new_lcov.info -o coverage/html
+fvm flutter test --no-test-assets --no-pub --concurrency="$CONCURRENCY" --coverage --branch-coverage
+lcov -q --rc branch_coverage=1 \
+  --remove coverage/lcov.info 'lib/domain/*' 'lib/application/app_bloc_observer.dart' \
+  -o coverage/new_lcov.info
+genhtml coverage/new_lcov.info -o coverage/html -s -q --branch-coverage --function-coverage
 open coverage/html/index.html
 
 echo "Tests completed on $(date)"
