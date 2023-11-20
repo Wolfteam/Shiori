7b39ce359924fa5683e740a8625ff144a7916014
Efrain Bastidas <mimo4325@gmail.com>
2023-11-20 15:04:29 -0500

    Updated run_tests_coverage.sh

--- run_tests_coverage.sh
+++ run_tests_coverage.sh
@@ -6,9 +6,14 @@ echo "Running tests with concurrency = $CONCURRENCY on $(date)"
 
 fvm flutter test --no-test-assets --no-pub --concurrency="$CONCURRENCY" --coverage --branch-coverage
 lcov -q --rc branch_coverage=1 \
-  --remove coverage/lcov.info 'lib/domain/*' 'lib/application/app_bloc_observer.dart' \
-  -o coverage/new_lcov.info
-genhtml coverage/new_lcov.info -o coverage/html -s -q --branch-coverage --function-coverage
+  --remove coverage/lcov.info 'lib/domain/*' 'lib/application/app_bloc_observer.dart' 'generated/*' 'lib/presentation/*' \
+  -o coverage/lcov.info
+genhtml coverage/lcov.info -o coverage/html -s -q --branch-coverage --function-coverage
 open coverage/html/index.html
+
+echo "Uploading to codecov..."
+# To run this line you need the CODECOV_TOKEN env var set
+# CODECOV_TOKEN=xxxxxxxxx-xxxxx-xxxx
+codecovcli upload-process
 
 echo "Tests completed on $(date)"
