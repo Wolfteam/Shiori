bdf5506f6b28d13661155c89033df0baec795f41
Efrain Bastidas <mimo4325@gmail.com>
2024-04-06 18:23:17 -0500

    [Tests] Test fixes

--- test/infrastructure/locale_service_test.dart
+++ test/infrastructure/locale_service_test.dart
@@ -5,23 +5,12 @@ import '../common.dart';
 
 void main() {
   final languages = AppLanguageType.values.toList();
-  group('Get character birthday', () {
-    test("check Bennet's birthday not using current year", () {
-      for (final lang in languages) {
-        final service = getLocaleService(lang);
-        final birthday = service.getCharBirthDate('02/29');
-        expect(birthday.day, equals(29));
-        expect(birthday.month, equals(DateTime.february));
-      }
-    });
-
-    test("check Bennet's birthday using current year", () {
-      for (final lang in languages) {
-        final service = getLocaleService(lang);
-        final birthday = service.getCharBirthDate('02/29', useCurrentYear: true);
-        expect(birthday.month, equals(DateTime.february));
-        expect(birthday.day, isIn([28, 29]));
-      }
-    });
+  test("check Bennet's birthday using current year", () {
+    for (final lang in languages) {
+      final service = getLocaleService(lang);
+      final birthday = service.getCharBirthDate('02/29');
+      expect(birthday.month, equals(DateTime.february));
+      expect(birthday.day, 29);
+    }
   });
 }
