ca6e36383c9553481c8aa64692ccaad4dd083c8b
Efrain Bastidas <mimo4325@gmail.com>
2024-06-24 16:13:34 -0500

    [Domain] Added util method to format bytes

---
+++ lib/domain/utils/format_utils.dart
@@ -0,0 +1,12 @@
+import 'dart:math';
+
+class FormatUtils {
+  static String formatBytes(int bytes, {int decimals = 2}) {
+    if (bytes <= 0) {
+      return '0 B';
+    }
+    const suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    final int i = (log(bytes) / log(1024)).floor();
+    return '${(bytes / pow(1024, i)).toStringAsFixed(decimals)} ${suffixes[i]}';
+  }
+}
