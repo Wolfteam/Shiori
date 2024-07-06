d5eb4f495f060f7719ba3554e619c9dd5ae3d300
Efrain Bastidas <mimo4325@gmail.com>
2024-07-05 22:10:51 -0500

    [macOS] Removed app center dependency

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,8 +1,5 @@
 import Cocoa
 import FlutterMacOS
-import AppCenter
-import AppCenterAnalytics
-import AppCenterCrashes
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
@@ -22,52 +19,10 @@ class AppDelegate: FlutterAppDelegate {
     public func methodChannelHandler(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         debugPrint(call.method)
         switch call.method {
-        case "start":
-            guard let args: [String: Any] = (call.arguments as? [String: Any]) else {
-                result(FlutterError(code: "400", message:  "Bad arguments", details: "iOS could not recognize flutter arguments in method: (start)") )
-                return
-            }
-            
-            let secret = args["secret"] as! String
-            AppCenter.start(withAppSecret: secret, services: [
-                Analytics.self,
-                Crashes.self,
-            ])
-        case "trackEvent":
-            trackEvent(call: call, result: result)
-            return
-        case "getInstallId":
-            result(AppCenter.installId.uuidString)
-            return
-        case "isCrashesEnabled":
-            result(Crashes.enabled)
-            return
-        case "configureCrashes":
-            Crashes.enabled = call.arguments as! Bool
-        case "isAnalyticsEnabled":
-            result(Analytics.enabled)
-            return
-        case "configureAnalytics":
-            Analytics.enabled = call.arguments as! Bool
         default:
             result(FlutterMethodNotImplemented);
             return
         }
         result(nil);
-    }
-    
-    private func trackEvent(call: FlutterMethodCall, result: FlutterResult) {
-        guard let args:[String: Any] = (call.arguments as? [String: Any]) else {
-            result(FlutterError(code: "400", message:  "Bad arguments", details: "iOS could not recognize flutter arguments in method: (trackEvent)") )
-            return
-        }
-        
-        let name = args["name"] as? String
-        let properties = args["properties"] as? [String: String]
-        if(name != nil) {
-            Analytics.trackEvent(name!, withProperties: properties)
-        }
-        
-        result(nil)
     }
 }
