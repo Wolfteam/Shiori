9dfd854d7140b340498a95e7a36132556e78f834
Efrain Bastidas <mimo4325@gmail.com>
2024-10-24 17:32:05 -0500

    [Domain] Updated register_device_token_request_dto.dart

--- lib/domain/models/dtos/requests/register_device_token_request_dto.dart
+++ lib/domain/models/dtos/requests/register_device_token_request_dto.dart
@@ -1,4 +1,5 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:shiori/domain/enums/enums.dart';
 import 'package:shiori/domain/models/dtos/requests/base_request_dto.dart';
 
 part 'register_device_token_request_dto.g.dart';
@@ -7,11 +8,18 @@ part 'register_device_token_request_dto.g.dart';
 class RegisterDeviceTokenRequestDto extends BaseRequestDto {
   final String token;
 
-  const RegisterDeviceTokenRequestDto({
+  @JsonKey(name: 'language', includeToJson: true)
+  final int languageIndex;
+
+  @JsonKey(includeToJson: false)
+  final AppLanguageType language;
+
+  RegisterDeviceTokenRequestDto({
     required super.appVersion,
     required super.currentVersion,
     required this.token,
-  });
+    required this.language,
+  }) : languageIndex = language.index;
 
   @override
   Map<String, dynamic> toJson() => _$RegisterDeviceTokenRequestDtoToJson(this);
