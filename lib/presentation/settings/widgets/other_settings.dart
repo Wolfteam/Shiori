e1b00591dcfe6ffcd1b506e126f3a907e20fca4b
Efrain Bastidas <mimo4325@gmail.com>
2025-06-07 15:23:38 -0500

    [Presentation] Minor loading ui bug fix

--- lib/presentation/settings/widgets/other_settings.dart
+++ lib/presentation/settings/widgets/other_settings.dart
@@ -35,89 +35,110 @@ class OtherSettings extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.only(top: 5),
-            child: Text(
-              s.generalSettings,
-              style: const TextStyle(color: Colors.grey),
-            ),
+            child: Text(s.generalSettings, style: const TextStyle(color: Colors.grey)),
           ),
           BlocBuilder<SettingsBloc, SettingsState>(
-            builder: (context, state) => state.map(
-              loading: (_) => const Loading(useScaffold: false),
-              loaded: (settingsState) => Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  SwitchListTile(
-                    title: Text(s.showCharacterDetails),
-                    value: settingsState.showCharacterDetails,
-                    onChanged: (newVal) => context.read<SettingsBloc>().add(SettingsEvent.showCharacterDetailsChanged(newValue: newVal)),
-                  ),
-                  SwitchListTile(
-                    title: Text(s.showWeaponDetails),
-                    value: settingsState.showWeaponDetails,
-                    onChanged: (newVal) => context.read<SettingsBloc>().add(SettingsEvent.showWeaponDetailsChanged(newValue: newVal)),
-                  ),
-                  if (Platform.isAndroid)
-                    SwitchListTile(
-                      title: Text(s.pressOnceAgainToExit),
-                      value: settingsState.doubleBackToClose,
-                      onChanged: (newVal) => context.read<SettingsBloc>().add(SettingsEvent.doubleBackToCloseChanged(newValue: newVal)),
-                    ),
-                  SwitchListTile(
-                    title: Text(s.useOfficialMap),
-                    value: settingsState.useOfficialMap,
-                    onChanged: (newVal) => context.read<SettingsBloc>().add(SettingsEvent.useOfficialMapChanged(newValue: newVal)),
-                  ),
-                  SwitchListTile(
-                    title: Text(s.use24HourFormatOnDates),
-                    value: settingsState.useTwentyFourHoursFormat,
-                    onChanged: (newVal) => context.read<SettingsBloc>().add(SettingsEvent.useTwentyFourHoursFormatChanged(newValue: newVal)),
-                  ),
-                  SwitchListTile(
-                    title: Text(s.checkForUpdatesOnStartup),
-                    value: settingsState.checkForUpdatesOnStartup,
-                    onChanged: (newVal) => context.read<SettingsBloc>().add(SettingsEvent.checkForUpdatesOnStartupChanged(newValue: newVal)),
-                  ),
-                  CommonDropdownButton<AppServerResetTimeType>(
-                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-                    subTitle: s.serverWhereYouPlay,
-                    hint: s.chooseServer,
-                    currentValue: settingsState.serverResetTime,
-                    values: EnumUtils.getTranslatedAndSortedEnum<AppServerResetTimeType>(
-                      AppServerResetTimeType.values,
-                      (val, _) => s.translateServerResetTimeType(val),
-                    ),
-                    onChanged: (v, context) => context.read<SettingsBloc>().add(SettingsEvent.serverResetTimeChanged(newValue: v)),
-                  ),
-                  RequiresDownloadedResourcesWidget(
-                    child: ListTile(
-                      title: Text(s.backups),
-                      subtitle: Text(s.createAndRestoreLocalBackups),
-                      onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => const BackupsPage())),
-                    ),
-                  ),
-                  ListTile(
-                    title: Text(s.deleteAllData, style: theme.textTheme.titleMedium!.copyWith(color: Colors.red, fontWeight: FontWeight.bold)),
-                    onTap: () => showDialog<bool?>(
-                      context: context,
-                      builder: (_) => ConfirmDialog(
-                        title: s.confirm,
-                        content: '${s.deleteAllDataWarningMsg}\n${s.confirmQuestion}',
+            builder:
+                (context, state) => state.map(
+                  loading: (_) => const Loading(useScaffold: false),
+                  loaded:
+                      (settingsState) => Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          SwitchListTile(
+                            title: Text(s.showCharacterDetails),
+                            value: settingsState.showCharacterDetails,
+                            onChanged:
+                                (newVal) =>
+                                    context.read<SettingsBloc>().add(SettingsEvent.showCharacterDetailsChanged(newValue: newVal)),
+                          ),
+                          SwitchListTile(
+                            title: Text(s.showWeaponDetails),
+                            value: settingsState.showWeaponDetails,
+                            onChanged:
+                                (newVal) =>
+                                    context.read<SettingsBloc>().add(SettingsEvent.showWeaponDetailsChanged(newValue: newVal)),
+                          ),
+                          if (Platform.isAndroid)
+                            SwitchListTile(
+                              title: Text(s.pressOnceAgainToExit),
+                              value: settingsState.doubleBackToClose,
+                              onChanged:
+                                  (newVal) =>
+                                      context.read<SettingsBloc>().add(SettingsEvent.doubleBackToCloseChanged(newValue: newVal)),
+                            ),
+                          SwitchListTile(
+                            title: Text(s.useOfficialMap),
+                            value: settingsState.useOfficialMap,
+                            onChanged:
+                                (newVal) =>
+                                    context.read<SettingsBloc>().add(SettingsEvent.useOfficialMapChanged(newValue: newVal)),
+                          ),
+                          SwitchListTile(
+                            title: Text(s.use24HourFormatOnDates),
+                            value: settingsState.useTwentyFourHoursFormat,
+                            onChanged:
+                                (newVal) => context.read<SettingsBloc>().add(
+                                  SettingsEvent.useTwentyFourHoursFormatChanged(newValue: newVal),
+                                ),
+                          ),
+                          SwitchListTile(
+                            title: Text(s.checkForUpdatesOnStartup),
+                            value: settingsState.checkForUpdatesOnStartup,
+                            onChanged:
+                                (newVal) => context.read<SettingsBloc>().add(
+                                  SettingsEvent.checkForUpdatesOnStartupChanged(newValue: newVal),
+                                ),
+                          ),
+                          CommonDropdownButton<AppServerResetTimeType>(
+                            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                            subTitle: s.serverWhereYouPlay,
+                            hint: s.chooseServer,
+                            currentValue: settingsState.serverResetTime,
+                            values: EnumUtils.getTranslatedAndSortedEnum<AppServerResetTimeType>(
+                              AppServerResetTimeType.values,
+                              (val, _) => s.translateServerResetTimeType(val),
+                            ),
+                            onChanged:
+                                (v, context) =>
+                                    context.read<SettingsBloc>().add(SettingsEvent.serverResetTimeChanged(newValue: v)),
+                          ),
+                          RequiresDownloadedResourcesWidget(
+                            loadingWidth: 48,
+                            child: ListTile(
+                              title: Text(s.backups),
+                              subtitle: Text(s.createAndRestoreLocalBackups),
+                              onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => const BackupsPage())),
+                            ),
+                          ),
+                          ListTile(
+                            title: Text(
+                              s.deleteAllData,
+                              style: theme.textTheme.titleMedium!.copyWith(color: Colors.red, fontWeight: FontWeight.bold),
+                            ),
+                            onTap:
+                                () => showDialog<bool?>(
+                                  context: context,
+                                  builder:
+                                      (_) => ConfirmDialog(
+                                        title: s.confirm,
+                                        content: '${s.deleteAllDataWarningMsg}\n${s.confirmQuestion}',
+                                      ),
+                                ).then((confirmed) {
+                                  if (confirmed == true && context.mounted) {
+                                    final toast = ToastUtils.of(context);
+                                    ToastUtils.showInfoToast(toast, s.deletingAllDataMsg);
+                                    Future.delayed(const Duration(seconds: 1)).then((value) {
+                                      if (context.mounted) {
+                                        context.read<MainBloc>().add(const MainEvent.deleteAllData());
+                                      }
+                                    });
+                                  }
+                                }),
+                          ),
+                        ],
                       ),
-                    ).then((confirmed) {
-                      if (confirmed == true && context.mounted) {
-                        final toast = ToastUtils.of(context);
-                        ToastUtils.showInfoToast(toast, s.deletingAllDataMsg);
-                        Future.delayed(const Duration(seconds: 1)).then((value) {
-                          if (context.mounted) {
-                            context.read<MainBloc>().add(const MainEvent.deleteAllData());
-                          }
-                        });
-                      }
-                    }),
-                  ),
-                ],
-              ),
-            ),
+                ),
           ),
         ],
       ),
--- lib/presentation/shared/requires_resources_widget.dart
+++ lib/presentation/shared/requires_resources_widget.dart
@@ -2,29 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shiori/application/bloc.dart';
 import 'package:shiori/presentation/shared/dialogs/check_for_resource_updates_dialog.dart';
+import 'package:shiori/presentation/shared/styles.dart';
 
 class RequiresDownloadedResourcesWidget extends StatelessWidget {
   final Widget child;
+  final double loadingWidth;
 
-  const RequiresDownloadedResourcesWidget({super.key, required this.child});
+  const RequiresDownloadedResourcesWidget({super.key, required this.child, this.loadingWidth = Styles.homeCardWidth});
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<SettingsBloc, SettingsState>(
-      builder: (context, state) => state.map(
-        loading: (_) => const CircularProgressIndicator(),
-        loaded: (state) => state.resourceVersion <= 0
-            ? GestureDetector(
-                onTap: () => showDialog(context: context, builder: (context) => const CheckForResourceUpdatesDialog()),
-                child: AbsorbPointer(
-                  child: Opacity(
-                    opacity: 0.5,
-                    child: child,
-                  ),
-                ),
-              )
-            : child,
-      ),
+      builder:
+          (context, state) => state.map(
+            loading: (_) => SizedBox.square(dimension: loadingWidth, child: const Center(child: CircularProgressIndicator())),
+            loaded:
+                (state) =>
+                    state.resourceVersion <= 0
+                        ? GestureDetector(
+                          onTap: () => showDialog(context: context, builder: (context) => const CheckForResourceUpdatesDialog()),
+                          child: AbsorbPointer(child: Opacity(opacity: 0.5, child: child)),
+                        )
+                        : child,
+          ),
     );
   }
 }
